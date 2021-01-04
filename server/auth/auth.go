ac6e7cef873d8580e0c7945822112ced74e72f85
alailsonko <aalailson3@gmail.com>
2021-01-04 06:40:41 -0300

    feat: implements validation data

--- server/auth/auth.go
+++ server/auth/auth.go
@@ -1,8 +1,10 @@
 package auth
 
 import (
+	"fmt"
 	"log"
 
+	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,6 +14,15 @@ type AccountModel struct {
 	Email                string `json:"email" xml:"email" form:"email"`
 	Password             string `json:"password" xml:"password" form:"password"`
 	PasswordConfirmation string `json:"passwordConfirmation" xml:"passwordConfirmation" form:"passwordConfirmation"`
+}
+
+func (am AccountModel) Validate() error {
+	return validation.ValidateStruct(&am,
+		validation.Field(&am.Username, validation.Required),
+		validation.Field(&am.Email, validation.Required),
+		validation.Field(&am.Password, validation.Required),
+		validation.Field(&am.PasswordConfirmation, validation.Required),
+	)
 }
 
 // JSONResponse - type for signup reponse
@@ -34,6 +45,8 @@ func SignUp(c *fiber.Ctx) error {
 	log.Println(am.Password)
 	log.Println(am.PasswordConfirmation)
 
+	err := am.Validate()
+	fmt.Println(err)
 	response := JSONResponse{StatusCode: 200, StatusMessage: "create successfully"}
 
 	c.JSON(response.StatusMessage)
--- server/go.mod
+++ server/go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/cosmtrek/air v1.21.2 // indirect
 	github.com/creack/pty v1.1.11 // indirect
 	github.com/fatih/color v1.10.0 // indirect
+	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible // indirect
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
 	github.com/gofiber/fiber v1.14.6
 	github.com/gofiber/fiber/v2 v2.3.2
 	github.com/imdario/mergo v0.3.11 // indirect
